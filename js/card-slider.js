afbafea01179e9bbc1593289374a7d730f03d310
ipekcoskun <ipekcoskun@omegabigdata.com>
2024-08-31 04:55:30 +0300

    card-slider img

--- js/card-slider.js
+++ js/card-slider.js
@@ -4,15 +4,15 @@ let sliderNumber = 0;
 
 function cardSlider(sliderNumber) {
     if (sliderNumber === 1) {
-        myDiv.src = "https://cdn.tatilsepeti.com/Files/Banner/0293/suenobelek850x2009.jpg";
+        myDiv.src = "https://cdn.tatilsepeti.com/Files/Banner/0036/250x180R.jpg";
     }
     else if (sliderNumber === 2) {
-        myDiv.src = "https://cdn.tatilsepeti.com/Files/Banner/0293/Nirvana-Hotels-23.08.24.jpg";
+        myDiv.src = "https://cdn.tatilsepeti.com/Files/Banner/0037/250x180.jpg";
     }
     else if (sliderNumber === 3) {
-        myDiv.src = "https://cdn.tatilsepeti.com/Files/Banner/0388/765x240sn-1.jpg";
-    }                                             
-}                                     
+        myDiv.src = "https://cdn.tatilsepeti.com/Files/Banner/0036/250x180_vizesizbalkan-R.jpg";
+    }
+}
 
 setInterval(() => {
     if (sliderNumber < 3)
@@ -24,36 +24,3 @@ setInterval(() => {
 
 }, 3000);
 
-
-
-//var AllImages = document.querySelector(".cards-images");
-//var imageWidth = document.querySelector(".timed-slider-cards").clientWidth;
-
-//// the index value of current page
-//var i = 0;
-
-////placeholder for our setInterval function
-//var timer = 0;
-
-//// start timer function
-//function Start() {
-
-//    timer = setInterval(function () {
-//        if (i < AllImages.children.length) {
-//            i++;
-//            AllImages.style.transform = "translateX(" + (-imageWidth * i) + "px)";
-//            if (i >= AllImages.children.length) {
-//                AllImages.style.transform = "translateX(0px)";
-//                i = 0;
-//            }
-//        }
-//    }, 3000);
-//}
-
-//// starting the timer
-//Start();
-
-////stop the timer
-//function Stop() {
-//    clearInterval(timer);
-//}
